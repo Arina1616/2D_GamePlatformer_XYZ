42f66d07531df919f3bebeadf41e502e75396feb
Lankan <s.o.a.m.o.n.e.y.24.gmail.com>
2025-12-01 19:13:58 +0100

    Movement_InputSystem

---
+++ Assets/scripts/Hero.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    private Vector2 _direction;
+    public void SetDirection(Vector2 direction)
+    {
+        _direction = direction;
+        // Debug.Log(_direction);
+    }
+
+    private void Update()
+    {
+        if (_direction.x != 0 || _direction.y != 0 )
+        {
+            Vector3 move = new Vector3(_direction.x, _direction.y, 0) * _speed * Time.deltaTime;
+            transform.position = transform.position + move;
+        }
+    }
+}
